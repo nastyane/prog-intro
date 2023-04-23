8be9c09cb6392abd64eac5fa8d14ac894e829f21
Nastyaaa <312898@niuitmo.ru>
2023-04-23 17:59:28 +0300

    modificate

--- java-solutions/SumDouble.java
+++ java-solutions/SumDouble.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class SumDouble {
+import java.util.Arrays;
+
+public class SumDouble {
+    public static void main(String[] args) {
+        double result = 0.0;
+        for (String arg : args) {
+            result += parse(arg);
+        }
+        System.out.println(result);
+    }
+
+    public static double parse(String arg) {
+        int leftInd = 0;
+        double result = 0;
+        for (int j = 0; j < arg.length(); j++) {
+            char now = arg.charAt(j);
+            char last = j > 0 ? arg.charAt(j - 1) : ' ';
+            if (Character.isWhitespace(now)) {
+                if (!Character.isWhitespace(last)) {
+                    String number = arg.substring(leftInd, j);
+                    result += Double.parseDouble(number);
+                }
+
+            } else {
+                if (Character.isWhitespace(last)) {
+                    leftInd = j;
+                }
+            }
+        }
+        if (!Character.isWhitespace(arg.charAt(arg.length() - 1))) {
+            String ch = arg.substring(leftInd);
+            result += Double.parseDouble(ch);
+        }
+        return (double) result;
+    }
 }
