39c67c087ef29f8f5dc11d9f9c729c46e2229729
Pavel Sharaev <mail@pechhenka.ru>
2023-04-23 18:16:57 +0300

    checked

--- java-solutions/SumDouble.java
+++ java-solutions/SumDouble.java
@@ -9,28 +9,30 @@ public class SumDouble {
         System.out.println(result);
     }
 
-    public static double parse(String arg) {
+    private static double parse(String arg) {
         int leftInd = 0;
         double result = 0;
-        for (int j = 0; j < arg.length(); j++) {
-            char now = arg.charAt(j);
-            char last = j > 0 ? arg.charAt(j - 1) : ' ';
+        for (int i = 0; i < arg.length(); i++) {
+            char now = arg.charAt(i);
+            char last = i > 0 ? arg.charAt(i - 1) : ' ';
             if (Character.isWhitespace(now)) {
                 if (!Character.isWhitespace(last)) {
-                    String number = arg.substring(leftInd, j);
+                    String number = arg.substring(leftInd, i);
                     result += Double.parseDouble(number);
                 }
 
             } else {
                 if (Character.isWhitespace(last)) {
-                    leftInd = j;
+                    leftInd = i;
                 }
             }
         }
+        // :NOTE: дублирование кода (излишний код)
         if (!Character.isWhitespace(arg.charAt(arg.length() - 1))) {
             String ch = arg.substring(leftInd);
             result += Double.parseDouble(ch);
         }
-        return (double) result;
+        // :NOTE: лишний cast
+        return result;
     }
 }
