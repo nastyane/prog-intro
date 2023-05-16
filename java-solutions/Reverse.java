102ee788dd6f99be72c905c4a4b6eff3bd8a5369
Nastyaaa <312898@niuitmo.ru>
2023-05-16 16:35:12 +0300

    Done

--- java-solutions/Reverse.java
+++ java-solutions/Reverse.java
@@ -5,39 +5,37 @@ import java.util.Scanner;
 
 public class Reverse {
     public static void main(String[] args) {
-        List<List<Integer>> list = new ArrayList<>();
-        Scanner in = new Scanner(System.in);
+        List<List<Integer>> numbers = readInput();
+        System.err.println("*".repeat(10));
+        printOutput(numbers);
 
+    }
+
+    static List<List<Integer>> readInput() {
+        List<List<Integer>> numbers = new ArrayList<>();
+
+        Scanner in = new Scanner(System.in);
         while (in.hasNextLine()) {
             String line = in.nextLine();
-            String[] s = line.split(" ");
-            for (String value : s) {
-              
-                int v = Integer.parseInt(value);
-                list.add(Collections.singletonList(v));
+            List<Integer> lineNumbers = new ArrayList<>();
+            Scanner lineScanner = new Scanner(line);
+            while (lineScanner.hasNextInt()) {
+                int number = lineScanner.nextInt();
+                lineNumbers.add(number);
             }
+            System.err.println(lineNumbers);
+            numbers.add(lineNumbers);
         }
-        System.out.print(list + " ");
-        int rows = list.size();
-        int cols = list.get(0).size();
-        int[][] arr = new int[rows][cols];
+        Collections.reverse(numbers);
+        return numbers;
+    }
 
-        // Печать двумерного массива
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = arr[i].length - 1; j >= 0; j--) {
-                System.out.print(arr[i][j] + " ");
-            }
+    static void printOutput(List<List<Integer>> numbers) {
+        for (List<Integer> lineNumbers : numbers) {
+                for (int i = lineNumbers.size() - 1; i >= 0; i--) {
+                    System.out.print(lineNumbers.get(i) + " ");
+                }
             System.out.println();
         }
     }
 }
-
-
-
