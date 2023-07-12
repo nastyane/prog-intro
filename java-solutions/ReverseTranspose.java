2fb53d7668db23d21e777167f51ef599f20dc080
Nastyaaa <312898@niuitmo.ru>
2023-07-12 21:18:21 +0300

    finish test

--- java-solutions/ReverseTranspose.java
+++ java-solutions/ReverseTranspose.java
@@ -1,12 +1,44 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class ReverseTranspose {
     public static void main(String[] args) {
         List<List<Integer>> numbers = readInput();
-        List<List<Integer>> transpose = turn(numbers);
+//        System.err.println("=".repeat(40));
+//        System.err.println("*".repeat(10) + " numbers:");
+//        dump(numbers);
+        List<List<Integer>> transpose = maxSize(numbers);
+//        System.err.println("*".repeat(10) + " transpose:");
+//        dump(transpose);
         printOutput(transpose);
+    }
+
+//    static void dump(List<List<Integer>> numbers) {
+//        for (List<Integer> row : numbers) {
+//            for (Integer integer : row) {
+//                if (integer == null) {
+//                    System.err.print("  ");
+//                } else {
+//                    System.err.print(integer + " ");
+//                }
+//            }
+//            System.err.println();
+//        }
+//    }
+
+    static void printOutput(List<List<Integer>> transpose) {
+        for (List<Integer> row : transpose) {
+            for (Integer integer : row) {
+                if (integer == null) {
+                    System.out.print("");
+                } else {
+                    System.out.print(integer + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     static List<List<Integer>> readInput() {
@@ -20,36 +52,32 @@ public class ReverseTranspose {
                 int number = lineScanner.nextInt();
                 lineNumbers.add(number);
             }
-            System.err.println(lineNumbers);
             numbers.add(lineNumbers);
         }
         return numbers;
     }
 
-
-    static List<List<Integer>> turn (List<List<Integer>> turnLeft) {
+    static List<List<Integer>> maxSize(List<List<Integer>> turnLeft) {
         int rows = turnLeft.size();
-        int colum = turnLeft.get(0).size();
-
-        List<List<Integer>> transpose = new ArrayList<>();
-        for (int i = 0; i < colum; i++) {
-            List<Integer> transposeRow = new ArrayList<>();
-            for (int j = 0; j < rows; j++) {
-                transposeRow.add(turnLeft.get(j).get(i));
+        int maxColums = 0;
+        for (int i = 0; i < rows; i++) {
+            if (maxColums < turnLeft.get(i).size()) {
+                maxColums = turnLeft.get(i).size();
             }
-            transpose.add(transposeRow);
+        }
+        List<List<Integer>> transpose = new ArrayList<>();
+        for (int col = 0; col < maxColums; col++) {
+            transpose.add(new ArrayList<>());
+            for (int row = 0; row < rows; row++) {
+                if (turnLeft.get(row).size() > col) {
+                    transpose.get(col).add(turnLeft.get(row).get(col));
+                } else {
+                    transpose.get(col).add(null);
+                }
+
+            }
         }
 
         return transpose;
-    }
-
-
-    static void printOutput(List<List<Integer>> numbers) {
-        for (List<Integer> row : numbers) {
-            for (Integer integer : row) {
-                System.out.print(integer + " ");
-            }
-            System.out.println();
-        }
     }
 }
